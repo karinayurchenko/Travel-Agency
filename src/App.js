c032739018436b142d536f8d5333eab18860e275
Karina <karina.yurchenko28@gmail.com>
2020-05-24 23:35:56 +0300

    [ADD] login,register,tours  empty pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 import Sidebar from './components/layout/sidebar';
 import Nav from './components/layout/nav';
-import { Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/register';
+import Profile from './pages/profile';
 
 function App() {
   return (
-
-    <div className="App">
-      <Sidebar />
-      <div className="main">
-        <Nav />
-        <div>
-          <Switch>
-            <Route exact="/" path="/"><Home /></Route>
-          </Switch>
+    <Switch>
+      <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
+      <Route path="/">
+        <div className="App">
+          <Sidebar />
+          <div className="main">
+            <Nav />
+            <div>
+              <Switch>
+                <Route exact path="/"><Home /></Route>
+                <Route exact path="/profile"><Profile /></Route>
+              </Switch>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-
+      </Route>
+    </Switch>
   );
 }
 
